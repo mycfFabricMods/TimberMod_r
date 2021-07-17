6f0cb839ea8514a82a7776978a5c674581bc2ba0
rlDruDo <ichundeli@gmail.com>
2021-07-17 11:54:05 +0200

    begin bxg integration

--- src/main/java/mycf/timber/mixin/axemixin.java
+++ src/main/java/mycf/timber/mixin/axemixin.java
@@ -1,8 +1,11 @@
 package mycf.timber.mixin;
 
 
+import com.google.common.collect.Sets;
+import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
 import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
@@ -17,6 +20,8 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Set;
 
 @Mixin(AxeItem.class)
@@ -25,6 +30,10 @@ public class axemixin extends MiningToolItem {
         super(attackDamage, attackSpeed, material, (Tag<Block>) effectiveBlocks, settings);
     }
     Integer mode = 1;
+
+    private static final HashSet<Block> BYGLogs = new HashSet<Block>();
+
+
     public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
         if(!world.isClient()){
             if(mode == 1){
@@ -43,13 +52,18 @@ public class axemixin extends MiningToolItem {
     }
 
     public boolean postMine(ItemStack stack, World world, BlockState state, BlockPos pos, LivingEntity miner) {
+        FabricLoader.getInstance().isModLoaded("byg");
+        FabricLoader.getInstance().getEntrypoints("some_mod_entrypoint",)
+
+
+
         if(mode == 2) {
             int i = 1;
             int ii = 1;
             for (int j = pos.getY(); j <= 255; j++) {
-                BlockState sta = world.getBlockState(pos.up(i));
+                BlockState staup = world.getBlockState(pos.up(i));
                 BlockState stadown = world.getBlockState(pos.down(ii));
-                if (sta.isIn(BlockTags.LOGS)) {
+                if (staup.isIn(BlockTags.LOGS)) {
                     i++;
                 } else if (stadown.isIn(BlockTags.LOGS)) {
                     ii++;
@@ -74,5 +88,18 @@ public class axemixin extends MiningToolItem {
             }
         }
         return true;
+    }
+
+
+
+    static {
+        BYGLogs = Sets.newHashSet((Object[])(BYGBlocks.ASPEN_LOG, BYGBlocks.BAOBAB_LOG, BYGBlocks.BLUE_ENCHANTED_LOG,
+                BYGBlocks.CHERRY_LOG, BYGBlocks.CIKA_LOG, BYGBlocks.CYPRESS_LOG, BYGBlocks.EBONY_LOG,
+                BYGBlocks.FIR_LOG, BYGBlocks.GREEN_ENCHANTED_LOG, BYGBlocks.HOLLY_LOG, BYGBlocks.JACARANDA_LOG,
+                BYGBlocks.MAHOGANY_LOG, BYGBlocks.MANGROVE_LOG, BYGBlocks.MAPLE_LOG, BYGBlocks.PALO_VERDE_LOG,
+                BYGBlocks.PINE_LOG, BYGBlocks.RAINBOW_EUCALYPTUS_LOG, BYGBlocks.REDWOOD_LOG, BYGBlocks.SKYRIS_LOG,
+                BYGBlocks.WILLOW_LOG, BYGBlocks.WITCH_HAZEL_LOG, BYGBlocks.ZELKOVA_LOG, BYGBlocks.SYTHIAN_STEM,
+                BYGBlocks.EMBUR_PEDU, BYGBlocks.PALM_LOG, BYGBlocks.LAMENT_LOG, BYGBlocks.WITHERING_OAK_LOG,
+                BYGBlocks.BULBIS_STEM, BYGBlocks.NIGHTSHADE_LOG, BYGBlocks.IMBUED_NIGHTSHADE_LOG, BYGBlocks.ETHER_LOG);
     }
 }
