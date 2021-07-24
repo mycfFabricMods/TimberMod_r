41c650dbaab9097a774432cb9ef28194500c2dee
rlDruDo <ichundeli@gmail.com>
2021-07-24 21:14:20 +0200

    :bug:

--- src/main/java/mycf/timber/mixin/axemixin.java
+++ src/main/java/mycf/timber/mixin/axemixin.java
@@ -52,33 +52,35 @@ public class axemixin extends MiningToolItem {
     }
 
     public boolean postMine(ItemStack stack, World world, BlockState state, BlockPos pos, LivingEntity miner) {
-        //FabricLoader.getInstance().isModLoaded("byg");
-        //FabricLoader.getInstance().getEntrypoints("some_mod_entrypoint",);
-
         if(mode == 2) {
-            int i = 1;
-            int ii = 1;
-            for (int j = pos.getY(); j <= 255; j++) {
-                BlockState staup = world.getBlockState(pos.up(i));
-                BlockState stadown = world.getBlockState(pos.down(ii));
-                if (staup.isIn(BlockTags.LOGS)) {
-                    i++;
-                } else if (stadown.isIn(BlockTags.LOGS)) {
-                    ii++;
-                } else {
-                    j = 266;
+            if(state.isIn(BlockTags.LOGS)) {
+                int i = 1;
+                int ii = 1;
+                for (int j = pos.getY(); j <= 255; j++) {
+                    BlockState staup = world.getBlockState(pos.up(i));
+                    BlockState stadown = world.getBlockState(pos.down(ii));
+                    if (staup.isIn(BlockTags.LOGS)) {
+                        i++;
+                    } else if (stadown.isIn(BlockTags.LOGS)) {
+                        ii++;
+                    } else {
+                        j = 266;
+                    }
                 }
-            }
-            for (int k = 1; k <= i - 1; k++) {
-                world.breakBlock(pos.up(k), true);
-            }
-            for (int k = 1; k <= ii - 1; k++) {
-                world.breakBlock(pos.down(k), true);
-            }
-            int damages = ii + i - 1;
-            if (!world.isClient && state.getHardness(world, pos) != 0.0F) {
-                stack.damage(damages, miner, (PlayerEntity) -> PlayerEntity.sendEquipmentBreakStatus(EquipmentSlot.MAINHAND));
-
+                for (int k = 1; k <= i - 1; k++) {
+                    world.breakBlock(pos.up(k), true);
+                }
+                for (int k = 1; k <= ii - 1; k++) {
+                    world.breakBlock(pos.down(k), true);
+                }
+                int damages = ii + i - 1;
+                if (!world.isClient && state.getHardness(world, pos) != 0.0F) {
+                    stack.damage(damages, miner, (PlayerEntity) -> PlayerEntity.sendEquipmentBreakStatus(EquipmentSlot.MAINHAND));
+                }
+            } else {
+                if (!world.isClient && state.getHardness(world, pos) != 0.0F) {
+                    stack.damage(1, miner, (PlayerEntity) -> PlayerEntity.sendEquipmentBreakStatus(EquipmentSlot.MAINHAND));
+                }
             }
         }else{
             if (!world.isClient && state.getHardness(world, pos) != 0.0F) {
@@ -87,18 +89,4 @@ public class axemixin extends MiningToolItem {
         }
         return true;
     }
-
-
-
-/*    static {
-        BYGLogs = Sets.newHashSet((Object[])(BYGBlocks.ASPEN_LOG, BYGBlocks.BAOBAB_LOG, BYGBlocks.BLUE_ENCHANTED_LOG,
-                BYGBlocks.CHERRY_LOG, BYGBlocks.CIKA_LOG, BYGBlocks.CYPRESS_LOG, BYGBlocks.EBONY_LOG,
-                BYGBlocks.FIR_LOG, BYGBlocks.GREEN_ENCHANTED_LOG, BYGBlocks.HOLLY_LOG, BYGBlocks.JACARANDA_LOG,
-                BYGBlocks.MAHOGANY_LOG, BYGBlocks.MANGROVE_LOG, BYGBlocks.MAPLE_LOG, BYGBlocks.PALO_VERDE_LOG,
-                BYGBlocks.PINE_LOG, BYGBlocks.RAINBOW_EUCALYPTUS_LOG, BYGBlocks.REDWOOD_LOG, BYGBlocks.SKYRIS_LOG,
-                BYGBlocks.WILLOW_LOG, BYGBlocks.WITCH_HAZEL_LOG, BYGBlocks.ZELKOVA_LOG, BYGBlocks.SYTHIAN_STEM,
-                BYGBlocks.EMBUR_PEDU, BYGBlocks.PALM_LOG, BYGBlocks.LAMENT_LOG, BYGBlocks.WITHERING_OAK_LOG,
-                BYGBlocks.BULBIS_STEM, BYGBlocks.NIGHTSHADE_LOG, BYGBlocks.IMBUED_NIGHTSHADE_LOG, BYGBlocks.ETHER_LOG);
-    }
- */
 }
