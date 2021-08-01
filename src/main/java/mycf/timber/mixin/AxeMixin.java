a9b5a24ded9b0ed28b71afdba1893de1d63ab0cd
rlDruDo <ichundeli@gmail.com>
2021-08-01 05:23:03 +0200

    added new method for checking and breaking blocks, added mode lock, added mode to nbt

--- src/main/java/mycf/timber/mixin/AxeMixin.java
+++ src/main/java/mycf/timber/mixin/AxeMixin.java
@@ -1,27 +1,21 @@
 package mycf.timber.mixin;
 
 
-import com.google.common.collect.Sets;
-import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
-import net.minecraft.block.Blocks;
 import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.item.*;
+import net.minecraft.item.AxeItem;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.MiningToolItem;
+import net.minecraft.item.ToolMaterial;
 import net.minecraft.tag.BlockTags;
 import net.minecraft.tag.Tag;
-import net.minecraft.text.LiteralText;
-import net.minecraft.text.Text;
-import net.minecraft.util.Hand;
-import net.minecraft.util.TypedActionResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 
-import java.util.Arrays;
-import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Set;
 
 @Mixin(AxeItem.class)
@@ -29,28 +23,105 @@ public class AxeMixin extends MiningToolItem {
     protected AxeMixin(float attackDamage, float attackSpeed, ToolMaterial material, Set<Block> effectiveBlocks, Settings settings) {
         super(attackDamage, attackSpeed, material, (Tag<Block>) effectiveBlocks, settings);
     }
+
+    /*
     Integer mode = 1;
 
     public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
         if(!world.isClient()){
             if(mode == 1){
                 mode = 2;
-                Text text = new LiteralText("Chop all");
-                user.sendMessage(text,true);
+                // Text text = new LiteralText("Chop all");
+                // user.sendMessage(text,true);
 
             }else{
                 mode = 1;
-                Text text = new LiteralText("Chop 1");
-                user.sendMessage(text,true);
+                // Text text = new LiteralText("Chop 1");
+                // user.sendMessage(text,true);
 
             }
         }
         return super.use(world, user, hand);
     }
+    */
 
+    // works
     public boolean postMine(ItemStack stack, World world, BlockState state, BlockPos pos, LivingEntity miner) {
-        if(mode == 2) {
-            if(state.isIn(BlockTags.LOGS)) {
+        boolean mode = stack.getOrCreateNbt().getBoolean("MYCFTimberMode");
+        if (mode) {
+            if (state.isIn(BlockTags.LOGS)) {
+                int damage = 1;
+                int maxDamage1 = stack.getMaxDamage(); // The Axes should never break any Blocks if they would have been broken before them
+                int maxDamage = stack.getDamage();
+                int trueDamage = Math.abs(maxDamage1 - maxDamage);
+
+                // straight trees -> but implementing big trees is not that hard
+                Iterator<BlockPos> iteratorUp = BlockPos.iterate(pos.add(0, 1, 0), pos.add(0, 255, 0)).iterator();
+                // Always starts at the bottom of the world and therefore cant break any blocks
+                // for loop solves this...
+                // Is there any way to reduce this to one loop? (for i in range(0,255): blockState(i) and BlockState(-i)
+                //Iterator<BlockPos> iteratorDown = BlockPos.iterate(pos.add(0, -1, 0), pos.add(0, -(pos.getY()), 0)).iterator();
+
+
+                // This always checks ALL Blocks from building height to bedrock...
+                // It also breaks blocks that are not connected!
+
+                /*
+                for(int i = 0; i < 256; i++) {
+
+                    if(world.getBlockState(pos.down(i)).isIn(BlockTags.LOGS) && damage < maxDamage) {
+                        world.breakBlock(pos.down(i), true);
+                        damage++;
+                    }
+
+                    if(world.getBlockState(pos.up(i)).isIn(BlockTags.LOGS) && damage < maxDamage) {
+                        world.breakBlock(pos.up(i), true);
+                        damage++;
+                    }
+                }
+
+                 */
+
+                do {
+                    BlockPos blockPos = iteratorUp.next();
+
+                    if (world.getBlockState(blockPos).isIn(BlockTags.LOGS) && damage < trueDamage) {
+                        world.breakBlock(blockPos, true);
+                        damage++;
+
+                    } else {
+                        break;
+                    }
+
+                } while (iteratorUp.hasNext());
+
+
+                // Best Option for checking all of the blocks below?
+                for(int i = 1; i < pos.getY() && world.getBlockState(pos.down(i)).isIn(BlockTags.LOGS) && damage < trueDamage; i++){
+                    world.breakBlock(pos.down(i), true);
+                    damage++;
+                }
+
+                /*
+                do {
+                    BlockPos blockPos2 = iteratorDown.next();
+
+                    if (world.getBlockState(blockPos2).isIn(BlockTags.LOGS)) {
+                        world.breakBlock(blockPos2, true);
+                        damage++;
+
+                    } else {
+                        break;
+                    }
+
+                } while (iteratorDown.hasNext() && damage <= maxDamage);
+                   */
+
+                if (!world.isClient && state.getHardness(world, pos) != 0.0F) {
+                    stack.damage(damage, miner, (PlayerEntity) -> PlayerEntity.sendEquipmentBreakStatus(EquipmentSlot.MAINHAND));
+
+                }
+        /*
                 int i = 1;
                 int ii = 1;
                 for (int j = pos.getY(); j <= 255; j++) {
@@ -74,16 +145,19 @@ public class AxeMixin extends MiningToolItem {
                 if (!world.isClient && state.getHardness(world, pos) != 0.0F) {
                     stack.damage(damages, miner, (PlayerEntity) -> PlayerEntity.sendEquipmentBreakStatus(EquipmentSlot.MAINHAND));
                 }
+         */
+                } else {
+                    if (!world.isClient && state.getHardness(world, pos) != 0.0F) {
+                        stack.damage(1, miner, (PlayerEntity) -> PlayerEntity.sendEquipmentBreakStatus(EquipmentSlot.MAINHAND));
+                    }
+                }
+
             } else {
                 if (!world.isClient && state.getHardness(world, pos) != 0.0F) {
                     stack.damage(1, miner, (PlayerEntity) -> PlayerEntity.sendEquipmentBreakStatus(EquipmentSlot.MAINHAND));
                 }
             }
-        }else{
-            if (!world.isClient && state.getHardness(world, pos) != 0.0F) {
-                stack.damage(1, miner, (PlayerEntity) -> PlayerEntity.sendEquipmentBreakStatus(EquipmentSlot.MAINHAND));
-            }
-        }
+
         return true;
     }
 }
--- src/main/java/mycf/timber/mixin/ItemStackMixin.java
+++ src/main/java/mycf/timber/mixin/ItemStackMixin.java
@@ -1,11 +1,15 @@
 package mycf.timber.mixin;
 
+import net.minecraft.block.Blocks;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.AxeItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemUsageContext;
 import net.minecraft.nbt.NbtCompound;
-import net.minecraft.nbt.NbtElement;
+import net.minecraft.text.LiteralText;
+import net.minecraft.text.TranslatableText;
+import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
 import net.minecraft.world.World;
@@ -15,21 +19,61 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+import java.util.Objects;
+
 @Mixin(ItemStack.class)
 public abstract class ItemStackMixin {
 
-    @Shadow public abstract Item getItem();
+    @Shadow
+    public abstract Item getItem();
 
-    @Shadow public abstract NbtCompound getOrCreateNbt();
+    @Shadow
+    public abstract NbtCompound getOrCreateNbt();
+
+    @Shadow
+    public abstract NbtCompound getNbt();
 
     @Inject(method = "use", at = @At(value = "HEAD"))
-     public void use(World world, PlayerEntity user, Hand hand, CallbackInfoReturnable<TypedActionResult<ItemStack>> cir) {
-        if(this.getItem() instanceof AxeItem){
-            NbtElement nbtElement = this.getOrCreateNbt().get("MYCFTimberMode");
-            // figure out how the nbt data is stored
-            // if(nbtElement != null && ){
+    public void use(World world, PlayerEntity user, Hand hand, CallbackInfoReturnable<TypedActionResult<ItemStack>> cir) {
+        if (this.getItem() instanceof AxeItem && !this.getOrCreateNbt().getBoolean("MYCFTimberModeNever")) {
+            if (!this.getOrCreateNbt().contains("MYCFTimberMode")) {
+                this.getOrCreateNbt().putBoolean("MYCFTimberMode", false);
+            }
 
-            // }
+            boolean bl = this.getNbt().getBoolean("MYCFTimberMode");
+            this.getOrCreateNbt().putBoolean("MYCFTimberMode", !bl);
+            if (bl) {
+                // maybe switch 'chop all' and 'chop 1'
+                user.sendMessage(new LiteralText("chop 1"), true);
+            } else {
+                user.sendMessage(new LiteralText("chop all"), true);
+            }
+        }
+    }
+
+
+    // TODO maybe set it to either mode
+    // TODO maybe add shift so you dont do that when building a base
+    @Inject(method = "useOnBlock", at = @At(value = "HEAD"))
+    public void useOnBlock(ItemUsageContext ctx, CallbackInfoReturnable<ActionResult> cir) {
+        if (this.getItem() instanceof AxeItem) {
+            if (!this.getOrCreateNbt().contains("MYCFTimberMode")) {
+                this.getOrCreateNbt().putBoolean("MYCFTimberMode", false);
+            }
+
+            if (!this.getOrCreateNbt().contains("MYCFTimberModeNever")) {
+                this.getOrCreateNbt().putBoolean("MYCFTimberMode", false);
+            }
+
+            if (ctx.getWorld().getBlockState(ctx.getBlockPos()).getBlock().equals(Blocks.POLISHED_GRANITE)) {
+                boolean bl = this.getNbt().getBoolean("MYCFTimberModeNever");
+                this.getOrCreateNbt().putBoolean("MYCFTimberModeNever", !bl);
+                this.getOrCreateNbt().putBoolean("MYCFTimberMode", false);
+                Objects.requireNonNull(ctx.getPlayer()).sendMessage(new TranslatableText("item.timber.axe.nevermode"), true);
+            }
+
         }
     }
 }
+
+
